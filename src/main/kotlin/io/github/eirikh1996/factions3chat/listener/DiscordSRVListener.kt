a3d2885c7e3c13f077791e41a4561b931d0f1592
eirikh1996 <eirikhetland1996@gmail.com>
2020-01-20 23:25:18 +0100

    Some improvements in DiscordSRV compat

--- src/main/kotlin/io/github/eirikh1996/factions3chat/listener/DiscordSRVListener.kt
+++ src/main/kotlin/io/github/eirikh1996/factions3chat/listener/DiscordSRVListener.kt
@@ -3,6 +3,7 @@ package io.github.eirikh1996.factions3chat.listener
 import com.massivecraft.factions.entity.MPlayerColl
 import github.scarsz.discordsrv.DiscordSRV
 import github.scarsz.discordsrv.api.Subscribe
+import github.scarsz.discordsrv.api.events.DiscordGuildMessagePostProcessEvent
 import github.scarsz.discordsrv.api.events.DiscordGuildMessagePreProcessEvent
 import github.scarsz.discordsrv.api.events.GameChatMessagePreProcessEvent
 import github.scarsz.discordsrv.util.DiscordUtil
@@ -30,7 +31,7 @@ class DiscordSRVListener {
     }
 
     @Subscribe
-    fun onMessageReceive(event : DiscordGuildMessagePreProcessEvent) {
+    fun onMessageReceive(event : DiscordGuildMessagePostProcessEvent) {
         val user = event.message.author
         val dsrv = Main.instance.discordSrvPlugin
         val pid = dsrv!!.accountLinkManager.getUuid(user.id)
@@ -46,13 +47,8 @@ class DiscordSRVListener {
         if (player == null) {
             return
         }
-        var nick : String
-        if (event.member.nickname == null) {
-            nick = event.member.effectiveName
-        } else {
-            nick = event.member.nickname!!
-        }
-        Bukkit.broadcast(ChatPrefixes.STAFF + " [ §bDiscord§r | " + event.member.roles[0].name+ "] " + nick + " > " +  event.message.contentDisplay, "factions3chat.staff")
+
+        Bukkit.broadcast(ChatPrefixes.STAFF +  event.processedMessage, "factions3chat.staff")
 
     }
 }
